1c9f7168c5e748362ca7debac04abec5ea6182d3
Farish Kashefinejad <Farish@Farishs-iMac.local>
2016-06-13 20:33:55 -0700

    style about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -1,17 +1,23 @@
 var React = require('react');
-// var About = React.createClass({
-//   render: function(){
-//     return(
-//       <h3> About Component</h3>
-//     );
-//   }
-// });
+
+
 
 var About = (props) => {
   return(
     <div>
-    <h3> About </h3>
-    <p>Welcome to the About Page</p>
+    <h1 className="text-center"> About </h1>
+      <ol>
+        <li>
+          <a href="https://facebook.github.io/react/">Built with React </a>
+        </li>
+        <li>
+          <a href="https://openweathermap.org">and OpenWeatherMap.Org </a>
+        </li>
+
+        <li>
+          <a href='https://github.com/farishkash/ReactWeather'>Source Code </a>
+        </li>
+      </ol>
     </div>
   )
 };
